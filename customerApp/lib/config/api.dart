3b5df78e615d1bf569d611eac036bd51518f17d3
yasser abu hejlah <58421265+yabuhejlah@users.noreply.github.com>
2021-05-25 12:01:30 +0300

    Delete customerApp directory

--- customerApp/lib/config/api.dart
+++
@@ -1,6 +0,0 @@
-final String _path = "https://ma7al.store/";
-
-
-String serverPath = "${_path}api/";
-String serverImages = "${_path}images/";
-String serverImgNoUserPath = "${_path}img/user.png";
--- customerApp/lib/model/dprint.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-// //dprint(String str){
-//   if (!kReleaseMode) print(str);
-// }
